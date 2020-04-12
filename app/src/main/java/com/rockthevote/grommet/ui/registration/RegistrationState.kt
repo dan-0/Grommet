4b95188e8f73f2ea315cc52976c6556c8b010c32
Dan Lowe <dan@danlowe.me>
2020-04-12 14:56:01 -0400

    Add abandonded state

--- app/src/main/java/com/rockthevote/grommet/ui/registration/RegistrationState.kt
+++ app/src/main/java/com/rockthevote/grommet/ui/registration/RegistrationState.kt
@@ -5,6 +5,7 @@ import androidx.annotation.StringRes
 sealed class RegistrationState {
     object Init: RegistrationState()
     object InProgress : RegistrationState()
+    object Abandoned : RegistrationState()
     object Complete : RegistrationState()
     class RegistrationError(
         val isAcknowledged: Boolean = false,
