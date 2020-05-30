31e5ceb89bd57029b562126206d311017cad2add
huttneab <huttneab@gmail.com>
2020-05-30 14:42:57 -0400

    update fake session dao

--- app/src/test/java/com/rockthevote/grommet/testdata/FakeSessionDao.kt
+++ app/src/test/java/com/rockthevote/grommet/testdata/FakeSessionDao.kt
@@ -1,7 +1,9 @@
 package com.rockthevote.grommet.testdata
 
+import androidx.lifecycle.LiveData
 import com.rockthevote.grommet.data.db.dao.SessionDao
 import com.rockthevote.grommet.data.db.model.Session
+import com.rockthevote.grommet.data.db.relationship.SessionWithPartnerInfo
 import com.rockthevote.grommet.data.db.relationship.SessionWithRegistrations
 
 class FakeSessionDao(
@@ -12,8 +14,19 @@ class FakeSessionDao(
     }
 
     override fun getCurrentSession() = getCurrentSessionResult.removeAt(0)
+    override fun getSessionWithPartnerInfo(): LiveData<SessionWithPartnerInfo?> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun getSessionWithRegistrations(): List<SessionWithRegistrations> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun insert(vararg session: Session) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun clearAllSessionInfo() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
